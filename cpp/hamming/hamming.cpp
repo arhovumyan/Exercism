cf5f9a4c8e6fa40df9eebc6c0bf79405ff6ac618
Arego11 <aregar20012001@gmail.com>
2026-07-21 12:12:39 -0700

    hamming submitted

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,7 +1,23 @@
 #include "hamming.h"
+#include <string>
+#include <iostream>
 
 namespace hamming {
+    int compute(const std::string& first, const std::string& second){
+        int count = 0;
+        if (first.size() != second.size()) throw std::domain_error("error");
 
-// TODO: add your solution here
-
+        for (std::size_t i = 0; i < first.size(); i++){
+            if (first[i] != second[i]) count++;
+        }
+        return count;
+    }
 }  // namespace hamming
+
+// int main () {
+//     //create an object comp
+//     int comp = hamming::compute("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT");
+
+//     std::cout << comp << std::endl;
+//     return 0;
+// }
--- cpp/hamming/hamming.h
+++ cpp/hamming/hamming.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <string>
 
-namespace hamming {
+// you will take 2 strings and iterate through them one by one and count in case 
+// chars of same indexes are not the same
 
-// TODO: add your solution here
-
+namespace hamming {    
+    int compute(const std::string& first, const std::string& second);
 }  // namespace hamming
