7d42f114c8edb7e413da0fb8a40272bea6319e14
Arego11 <aregar20012001@gmail.com>
2026-07-20 18:09:44 -0700

    power of troy added

--- cpp/doctor-data/doctor_data.cpp
+++ cpp/doctor-data/doctor_data.cpp
@@ -8,32 +8,31 @@
 
 namespace heaven {
 
-            Vessel::Vessel(std::string vessel_name, int vessel_generation, star_map::System vessel_system)
-              : name{vessel_name},
-                generation{vessel_generation},
-                current_system{vessel_system}
-                {}
-                
-            Vessel Vessel::replicate(std::string new_name){
-                return Vessel {new_name, generation + 1, current_system};
-            }
-            
-            void Vessel::make_buster(){busters++;}
-            bool Vessel::shoot_buster() {
+    Vessel::Vessel(std::string vessel_name, int vessel_generation, star_map::System vessel_system)
+        : name{vessel_name},
+        generation{vessel_generation},
+        current_system{vessel_system}
+        {}
+        
+    Vessel Vessel::replicate(std::string new_name){
+        return Vessel {new_name, generation + 1, current_system};
+    }
+
+    void Vessel::make_buster(){busters++;}
+    bool Vessel::shoot_buster() {
     if (busters > 0) {
         busters--;
         return true;
     }
-
     return false;
 }
 
-            std::string get_older_bob(const Vessel& name1, const Vessel& name2){
-                if  (name1.generation < name2.generation) {return name1.name;}
-                return name2.name;
-            }
-            bool in_the_same_system(const Vessel& system1, const Vessel& system2){
-                return system1.current_system == system2.current_system;
-            }
+    std::string get_older_bob(const Vessel& name1, const Vessel& name2){
+        if  (name1.generation < name2.generation) {return name1.name;}
+        return name2.name;
+    }
+    bool in_the_same_system(const Vessel& system1, const Vessel& system2){
+        return system1.current_system == system2.current_system;
+    }
 
 }
