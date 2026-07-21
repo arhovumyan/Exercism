4773d3feb5f72b5f2b6fc2978cc588ee72aba910
Arego11 <aregar20012001@gmail.com>
2026-07-21 11:22:56 -0700

    grade school finished with structs

--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,18 +1,36 @@
+/*
+Create a roster for the school
+
+1) add students to the roster
+    each student should be added to its own grade. 
+2) retrieve a list of students in the given grade.
+3) get a sorted list of all students in all grades.
+    sorted alphabetically and numerically increasingly
+
+each student name can be only added once
+
+
+*/
+
 #pragma once
 #include <map>
 #include <vector>
 #include <string>
 
 namespace grade_school {
-
-class school {
-private:
-    std::map<int,std::vector<std::string>> students;
-
-public:
-    void add(const std::string& name, int grade_number);
-    std::vector<std::string> grade(int grade_number) const;
-    std::vector<std::string> roster() const ;
+    
+struct Student {
+    std::string studentName;
+    int studentGrade;
 };
 
+struct school {
+private:
+    std::vector<Student> students_;
+
+public:
+    void add(const std::string& name, int grade);
+    std::vector<std::string> grade(int gradeNumber) const;
+    std::map<int, std::vector<std::string>> roster() const;
+};
 }  // namespace grade_school
